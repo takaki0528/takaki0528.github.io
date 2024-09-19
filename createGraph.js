2666d6f09219394522846609e5676d20ccf3dddc
takaki0528 <ozimatakaki@gmail.com>
2024-09-19 18:35:17 +0000

    add Ave and All

--- createGraph.js
+++ createGraph.js
@@ -1,6 +1,6 @@
 var device_name = "bms_multiple_battery";
 var ba_ids = ["ba0", "ba1", "ba2", "ba3"];
-var selectedBaId = "ba0";  // 初期値は ba0
+var selectedBaId = "Ave";  // 初期値は ba0
 var hosturl = "https://4bbamgyg6f.execute-api.ap-northeast-1.amazonaws.com/bms";
 var apiurl = hosturl + "/datas/" + device_name;
 var retryInterval = 60000;
@@ -25,7 +25,13 @@ function reqGet() {
         url: apiurl,
         method: "GET",
         success: function(data) {
-            drawChartsForSelectedBA(data[device_name], selectedBaId);
+            if (selectedBaId === "Ave") {
+                drawChartsForAverage(data[device_name]);
+            } else if (selectedBaId === "All") {
+                drawChartsForAllBA(data[device_name]);
+            } else {
+                drawChartsForSelectedBA(data[device_name], selectedBaId);
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
             console.log("Error: " + textStatus + " " + errorThrown);
@@ -44,38 +50,83 @@ function drawChartsForSelectedBA(vals, ba_id) {
         temp_data.push([vals[i].timestamp, vals[i][ba_id].temperature]);
     }
 
-    // 平均化処理
-    var windowSize = 1;
-    voltage_data = calculateMovingAverage(voltage_data, windowSize);
-    current_data = calculateMovingAverage(current_data, windowSize);
-    soc_data = calculateMovingAverage(soc_data, windowSize);
-    temp_data = calculateMovingAverage(temp_data, windowSize);
-
-    // X軸とY軸を反転する処理を追加
-    voltage_data = voltage_data.reverse();
-    current_data = current_data.reverse();
-    soc_data = soc_data.reverse();
-    temp_data = temp_data.reverse();
-
     drawChart("voltageChart", "Voltage (V)", voltage_data);
     drawChart("currentChart", "Current (A)", current_data);
     drawChart("socChart", "SOC (%)", soc_data);
     drawChart("tempChart", "Temperature (C)", temp_data);
 }
 
-function calculateMovingAverage(data, windowSize) {
-    let result = [];
-    for (let i = 0; i < data.length - windowSize + 1; i++) {
-        let sum = 0;
-        for (let j = i; j < i + windowSize; j++) {
-            sum += data[j][1];
-        }
-        result.push([data[i][0], sum / windowSize]);
+// 4つのバッテリーの平均データを描画
+function drawChartsForAverage(vals) {
+    var voltage_data = [], current_data = [], soc_data = [], temp_data = [];
+
+    for (var i = 0; i < vals.length; i++) {
+        var avg_voltage = 0, avg_current = 0, avg_soc = 0, avg_temp = 0;
+        ba_ids.forEach(function(ba_id) {
+            avg_voltage += vals[i][ba_id].voltage;
+            avg_current += vals[i][ba_id].current;
+            avg_soc += vals[i][ba_id].soc * 100;
+            avg_temp += vals[i][ba_id].temperature;
+        });
+        avg_voltage /= ba_ids.length;
+        avg_current /= ba_ids.length;
+        avg_soc /= ba_ids.length;
+        avg_temp /= ba_ids.length;
+
+        voltage_data.push([vals[i].timestamp, avg_voltage]);
+        current_data.push([vals[i].timestamp, avg_current]);
+        soc_data.push([vals[i].timestamp, avg_soc]);
+        temp_data.push([vals[i].timestamp, avg_temp]);
     }
-    return result;
+
+    drawChart("voltageChart", "Average Voltage (V)", voltage_data);
+    drawChart("currentChart", "Average Current (A)", current_data);
+    drawChart("socChart", "Average SOC (%)", soc_data);
+    drawChart("tempChart", "Average Temperature (C)", temp_data);
 }
 
-function drawChart(chartId, yAxisTitle, data) {
+// すべてのバッテリーのデータを同時に描画
+function drawChartsForAllBA(vals) {
+    var voltage_data = {}, current_data = {}, soc_data = {}, temp_data = {};
+
+    ba_ids.forEach(function(ba_id) {
+        voltage_data[ba_id] = [];
+        current_data[ba_id] = [];
+        soc_data[ba_id] = [];
+        temp_data[ba_id] = [];
+    });
+
+    for (var i = 0; i < vals.length; i++) {
+        ba_ids.forEach(function(ba_id) {
+            voltage_data[ba_id].push([vals[i].timestamp, vals[i][ba_id].voltage]);
+            current_data[ba_id].push([vals[i].timestamp, vals[i][ba_id].current]);
+            soc_data[ba_id].push([vals[i].timestamp, vals[i][ba_id].soc * 100]);
+            temp_data[ba_id].push([vals[i].timestamp, vals[i][ba_id].temperature]);
+        });
+    }
+
+    drawChartForAll("voltageChart", "Voltage (V)", voltage_data);
+    drawChartForAll("currentChart", "Current (A)", current_data);
+    drawChartForAll("socChart", "SOC (%)", soc_data);
+    drawChartForAll("tempChart", "Temperature (C)", temp_data);
+}
+
+// 全バッテリーのデータを同時に描画するための関数
+function drawChartForAll(chartId, yAxisTitle, data) {
+    var series = [];
+    ba_ids.forEach(function(ba_id) {
+        series.push({
+            name: ba_id,
+            type: 'line',
+            data: data[ba_id].map(item => item[1]),
+            tooltip: {
+                valueFormatter: function(value) {
+                    return value.toFixed(3);
+                }
+            }
+        });
+    });
+
     var chart = echarts.init(document.getElementById(chartId));
     chart.setOption({
         title: {
@@ -98,29 +149,20 @@ function drawChart(chartId, yAxisTitle, data) {
             }
         },
         legend: {
-            data: [selectedBaId],
+            data: ba_ids,
             top: '10%',
             right: 'center'
         },
         grid: {
             top: '20%'
         },
-        xAxis: { type: 'category', data: data.map(item => item[0]) },
+        xAxis: { type: 'category', data: data[ba_ids[0]].map(item => item[0]) },
         yAxis: {
             type: 'value',
             name: yAxisTitle,
             scale: true
         },
-        series: [{
-            name: selectedBaId,
-            type: 'line',
-            data: data.map(item => item[1]),
-            tooltip: {
-                valueFormatter: function(value) {
-                    return value.toFixed(3);
-                }
-            }
-        }]
+        series: series
     });
 
     window.addEventListener('resize', function() {
