2c4cf0ff0ea6b309fd055a2025259a09b8d5198a
root <ozimatakaki@gmail.com>
2024-09-20 02:26:21 +0000

    グラフリフレッシュを変更

--- createGraph.js
+++ createGraph.js
@@ -5,12 +5,11 @@ var hosturl = "https://4bbamgyg6f.execute-api.ap-northeast-1.amazonaws.com/bms";
 var apiurl = hosturl + "/datas/" + device_name;
 var retryInterval = 10000;
 var windowSize = 1;  // 平均化処理のウィンドウサイズ
-var charts = {};  // グラフのインスタンスを保持
 
 function createChart() {
     reqGet();  // まず一度実行
     setInterval(function () {
-        reqGet();  // 定期的にデータを取得して更新
+        reqGet();  // 60秒ごとに再度データ取得
     }, retryInterval);
 }
 
@@ -18,7 +17,6 @@ function createChart() {
 function handleBatteryChange() {
     var selectElement = document.getElementById("batterySelect");
     selectedBaId = selectElement.value;  // 新しいバッテリーIDを取得
-    clearCharts();  // グラフをリセット
     reqGet();  // 新しいバッテリーのデータを取得して更新
 }
 
@@ -29,11 +27,11 @@ function reqGet() {
         method: "GET",
         success: function (data) {
             if (selectedBaId === "Ave") {
-                updateChartsForAverage(data[device_name]);
+                drawChartsForAverage(data[device_name]);
             } else if (selectedBaId === "All") {
-                updateChartsForAllBA(data[device_name]);
+                drawChartsForAllBA(data[device_name]);
             } else {
-                updateChartsForSelectedBA(data[device_name], selectedBaId);
+                drawChartsForSelectedBA(data[device_name], selectedBaId);
             }
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -42,16 +40,8 @@ function reqGet() {
     });
 }
 
-// グラフをクリアする関数
-function clearCharts() {
-    if (charts.voltageChart) charts.voltageChart.clear();
-    if (charts.currentChart) charts.currentChart.clear();
-    if (charts.socChart) charts.socChart.clear();
-    if (charts.tempChart) charts.tempChart.clear();
-}
-
-// 選択されたバッテリーIDに基づいてデータを更新
-function updateChartsForSelectedBA(vals, ba_id) {
+// 選択されたバッテリーIDに基づいてデータを描画
+function drawChartsForSelectedBA(vals, ba_id) {
     var voltage_data = [], current_data = [], soc_data = [], temp_data = [];
 
     for (var i = 0; i < vals.length; i++) {
@@ -73,14 +63,14 @@ function updateChartsForSelectedBA(vals, ba_id) {
         updateLatestValues(latestData);  // 単一のバッテリーの最新値を更新
     }
 
-    updateChart("voltageChart", "Voltage (V)", voltage_data);
-    updateChart("currentChart", "Current (A)", current_data);
-    updateChart("socChart", "SOC (%)", soc_data);
-    updateChart("tempChart", "Temperature (℃)", temp_data);
+    drawChart("voltageChart", "Voltage (V)", voltage_data);
+    drawChart("currentChart", "Current (A)", current_data);
+    drawChart("socChart", "SOC (%)", soc_data);
+    drawChart("tempChart", "Temperature (℃)", temp_data);
 }
 
-// 4つのバッテリーの平均データを更新
-function updateChartsForAverage(vals) {
+// 4つのバッテリーの平均データを描画
+function drawChartsForAverage(vals) {
     var voltage_data = [], current_data = [], soc_data = [], temp_data = [];
 
     for (var i = 0; i < vals.length; i++) {
@@ -119,14 +109,14 @@ function updateChartsForAverage(vals) {
         updateLatestValues(latestAverageData);  // 平均値を更新
     }
 
-    updateChart("voltageChart", "Average Voltage (V)", voltage_data);
-    updateChart("currentChart", "Average Current (A)", current_data);
-    updateChart("socChart", "Average SOC (%)", soc_data);
-    updateChart("tempChart", "Average Temperature (℃)", temp_data);
+    drawChart("voltageChart", "Average Voltage (V)", voltage_data);
+    drawChart("currentChart", "Average Current (A)", current_data);
+    drawChart("socChart", "Average SOC (%)", soc_data);
+    drawChart("tempChart", "Average Temperature (℃)", temp_data);
 }
 
-// すべてのバッテリーのデータを同時に更新
-function updateChartsForAllBA(vals) {
+// すべてのバッテリーのデータを同時に描画
+function drawChartsForAllBA(vals) {
     var voltage_data = {}, current_data = {}, soc_data = {}, temp_data = {};
 
     // 各バッテリーのデータを初期化
@@ -155,136 +145,74 @@ function updateChartsForAllBA(vals) {
         temp_data[ba_id] = calculateMovingAverage(temp_data[ba_id], windowSize);
     });
 
-    updateChartForAll("voltageChart", "Voltage (V)", voltage_data);
-    updateChartForAll("currentChart", "Current (A)", current_data);
-    updateChartForAll("socChart", "SOC (%)", soc_data);
-    updateChartForAll("tempChart", "Temperature (℃)", temp_data);
+    drawChartForAll("voltageChart", "Voltage (V)", voltage_data);
+    drawChartForAll("currentChart", "Current (A)", current_data);
+    drawChartForAll("socChart", "SOC (%)", soc_data);
+    drawChartForAll("tempChart", "Temperature (℃)", temp_data);
 }
 
-// グラフに新しいデータを追加する関数
-function updateChart(chartId, yAxisTitle, data) {
-    if (!charts[chartId]) {
-        charts[chartId] = echarts.init(document.getElementById(chartId));
-        charts[chartId].setOption({
-            title: {
-                text: yAxisTitle + ' Over Time',
-                left: 'center',
-                top: '5%',
-                textStyle: {
-                    fontSize: 16,
-                    padding: [10, 0, 0, 0]
-                }
-            },
-            tooltip: {
-                trigger: 'axis',
-                formatter: function (params) {
-                    let result = params[0].axisValueLabel + '<br/>';
-                    params.forEach(item => {
-                        result += item.marker + ' ' + item.seriesName + ': ' + item.data.toFixed(3) + '<br/>';
-                    });
-                    return result;
-                }
-            },
-            legend: {
-                data: [selectedBaId],
-                top: '10%',
-                right: 'center'
-            },
-            grid: {
-                top: '20%'
-            },
-            xAxis: { type: 'category', data: data.map(item => item[0]) },
-            yAxis: {
-                type: 'value',
-                name: yAxisTitle,
-                scale: true
-            },
-            series: [{
-                name: selectedBaId,
-                type: 'line',
-                data: data.map(item => item[1]),
-                tooltip: {
-                    valueFormatter: function (value) {
-                        return value.toFixed(3);
-                    }
-                }
-            }]
-        });
-    } else {
-        charts[chartId].setOption({
-            xAxis: { data: data.map(item => item[0]) },
-            series: [{
-                data: data.map(item => item[1])
-            }]
-        });
+// すべてのバッテリーを同時に描画するための関数
+function drawChartForAll(chartId, yAxisTitle, data) {
+    // 既存のグラフインスタンスがあれば破棄
+    if (echarts.getInstanceByDom(document.getElementById(chartId))) {
+        echarts.dispose(document.getElementById(chartId));  // グラフをクリア
     }
-}
 
-// すべてのバッテリーのデータを同時に更新するための関数
-function updateChartForAll(chartId, yAxisTitle, data) {
-    if (!charts[chartId]) {
-        charts[chartId] = echarts.init(document.getElementById(chartId));
-        var series = [];
-        ba_ids.forEach(function (ba_id) {
-            series.push({
-                name: ba_id,
-                type: 'line',
-                data: data[ba_id].map(item => item[1]),  // 各バッテリーのデータを使用
-                tooltip: {
-                    valueFormatter: function (value) {
-                        return value.toFixed(3);
-                    }
-                }
-            });
-        });
-        charts[chartId].setOption({
-            title: {
-                text: yAxisTitle + ' Over Time',
-                left: 'center',
-                top: '5%',
-                textStyle: {
-                    fontSize: 16,
-                    padding: [10, 0, 0, 0]
-                }
-            },
+    var series = [];
+    ba_ids.forEach(function (ba_id) {
+        series.push({
+            name: ba_id,
+            type: 'line',
+            data: data[ba_id].map(item => item[1]),  // 各バッテリーのデータを使用
             tooltip: {
-                trigger: 'axis',
-                formatter: function (params) {
-                    let result = params[0].axisValueLabel + '<br/>';
-                    params.forEach(item => {
-                        result += item.marker + ' ' + item.seriesName + ': ' + item.data.toFixed(3) + '<br/>';
-                    });
-                    return result;
+                valueFormatter: function (value) {
+                    return value.toFixed(3);
                 }
-            },
-            legend: {
-                data: ba_ids,
-                top: '10%',
-                right: 'center'
-            },
-            grid: {
-                top: '20%'
-            },
-            xAxis: { type: 'category', data: data[ba_ids[0]].map(item => item[0]) },
-            yAxis: {
-                type: 'value',
-                name: yAxisTitle,
-                scale: true
-            },
-            series: series
+            }
         });
-    } else {
-        var series = [];
-        ba_ids.forEach(function (ba_id) {
-            series.push({
-                data: data[ba_id].map(item => item[1])
-            });
-        });
-        charts[chartId].setOption({
-            xAxis: { data: data[ba_ids[0]].map(item => item[0]) },
-            series: series
-        });
-    }
+    });
+
+    var chart = echarts.init(document.getElementById(chartId));
+    chart.setOption({
+        title: {
+            text: yAxisTitle + ' Over Time',
+            left: 'center',
+            top: '5%',
+            textStyle: {
+                fontSize: 16,
+                padding: [10, 0, 0, 0]
+            }
+        },
+        tooltip: {
+            trigger: 'axis',
+            formatter: function (params) {
+                let result = params[0].axisValueLabel + '<br/>';
+                params.forEach(item => {
+                    result += item.marker + ' ' + item.seriesName + ': ' + item.data.toFixed(3) + '<br/>';
+                });
+                return result;
+            }
+        },
+        legend: {
+            data: ba_ids,
+            top: '10%',
+            right: 'center'
+        },
+        grid: {
+            top: '20%'
+        },
+        xAxis: { type: 'category', data: data[ba_ids[0]].map(item => item[0]) },
+        yAxis: {
+            type: 'value',
+            name: yAxisTitle,
+            scale: true
+        },
+        series: series
+    });
+
+    window.addEventListener('resize', function () {
+        chart.resize();
+    });
 }
 
 // 最新の平均値を表示するための関数
@@ -306,6 +234,65 @@ function calculateMovingAverage(data, windowSize) {
         result.push([data[i][0], sum / windowSize]);
     }
     return result;
+}
+
+// 単一のバッテリーや平均データを描画するための基本的な関数
+function drawChart(chartId, yAxisTitle, data) {
+    // 既存のグラフインスタンスがある場合は破棄
+    if (echarts.getInstanceByDom(document.getElementById(chartId))) {
+        echarts.dispose(document.getElementById(chartId));  // グラフをクリア
+    }
+
+    var chart = echarts.init(document.getElementById(chartId));
+    chart.setOption({
+        title: {
+            text: yAxisTitle + ' Over Time',
+            left: 'center',
+            top: '5%',
+            textStyle: {
+                fontSize: 16,
+                padding: [10, 0, 0, 0]
+            }
+        },
+        tooltip: {
+            trigger: 'axis',
+            formatter: function (params) {
+                let result = params[0].axisValueLabel + '<br/>';
+                params.forEach(item => {
+                    result += item.marker + ' ' + item.seriesName + ': ' + item.data.toFixed(3) + '<br/>';
+                });
+                return result;
+            }
+        },
+        legend: {
+            data: [selectedBaId],
+            top: '10%',
+            right: 'center'
+        },
+        grid: {
+            top: '20%'
+        },
+        xAxis: { type: 'category', data: data.map(item => item[0]) },
+        yAxis: {
+            type: 'value',
+            name: yAxisTitle,
+            scale: true
+        },
+        series: [{
+            name: selectedBaId,
+            type: 'line',
+            data: data.map(item => item[1]),
+            tooltip: {
+                valueFormatter: function (value) {
+                    return value.toFixed(3);
+                }
+            }
+        }]
+    });
+
+    window.addEventListener('resize', function () {
+        chart.resize();
+    });
 }
 
 createChart();
